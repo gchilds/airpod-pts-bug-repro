c6c66cd9ff85459f3c02a7614bec926ff69aed09
Gordon Childs <gordon.childs@gmail.com>
2015-11-23 10:12:44 +1100

    what it was, basically http://stackoverflow.com/questions/33859290/cmsamplebufferref-parameter-in-delegate-callback-is-empty

--- objc-osx-avscreencap/AppDelegate.m
+++ objc-osx-avscreencap/AppDelegate.m
@@ -7,8 +7,12 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AppDelegate ()
+
+@property (nonatomic) AVCaptureSession *captureSession;
+@property (nonatomic) AVCaptureScreenInput *captureScreenInput;
 
 @end
 
@@ -20,6 +24,45 @@
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
 	// Insert code here to tear down your application
+}
+
+- (void)captureSessionRuntimeErrorDidOccur:(NSNotification *)not {
+	NSLog(@"SDFDSF %@", not);
+}
+
+- (BOOL)createCaptureSession:(NSError **)outError{
+	/* Create a capture session. */
+	self.captureSession = [[AVCaptureSession alloc] init];
+	if ([self.captureSession canSetSessionPreset:AVCaptureSessionPresetHigh])
+	{
+		/* Specifies capture settings suitable for high quality video and audio output. */
+		[self.captureSession setSessionPreset:AVCaptureSessionPresetHigh];
+	}
+	
+	/* Add the main display as a capture input. */
+	CGDirectDisplayID display = CGMainDisplayID();
+	self.captureScreenInput = [[AVCaptureScreenInput alloc] initWithDisplayID:display];
+	if ([self.captureSession canAddInput:self.captureScreenInput])
+	{
+		[self.captureSession addInput:self.captureScreenInput];
+	}
+	else
+	{   NSLog(@"nevyslo");
+		return NO;
+	}
+	
+	/* Register for notifications of errors during the capture session so we can display an alert. */
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(captureSessionRuntimeErrorDidOccur:) name:AVCaptureSessionRuntimeErrorNotification object:self.captureSession];
+	
+	
+	AVCaptureVideoDataOutput *output = [[AVCaptureVideoDataOutput alloc] init];
+	[self.captureSession addOutput:output];
+	
+	dispatch_queue_t queue = dispatch_queue_create("myQueue", NULL);
+	[output setSampleBufferDelegate:self queue:queue];
+	
+	[self.captureSession startRunning];
+	return YES;
 }
 
 @end

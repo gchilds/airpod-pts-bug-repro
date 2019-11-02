ba927ba6b3cbd780bc322a0c6bf0aea516898339
Gordon Childs <gchilds@Gordons-MacBook-Pro.local>
2019-11-02 11:34:44 +0100

    repro for 10.13

--- objc-osx-avscreencap/ScreenCapture.m
+++ objc-osx-avscreencap/ScreenCapture.m
@@ -6,10 +6,12 @@
 //  Copyright © 2015 Gordon Childs. All rights reserved.
 //
 
+// checking out sample buffer bug with airpods!
+
 #import "ScreenCapture.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface ScreenCapture() <AVCaptureVideoDataOutputSampleBufferDelegate>
+@interface ScreenCapture() <AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate>
 
 @property (nonatomic) AVCaptureSession *captureSession;
 
@@ -23,9 +25,40 @@
 	if (self) {
 		self.captureSession = [[AVCaptureSession alloc] init];
 		
-		AVCaptureScreenInput *input = [[AVCaptureScreenInput alloc] initWithDisplayID:CGMainDisplayID()];
-		[self.captureSession addInput:input];
+        if (0) {
+            AVCaptureScreenInput *screenInput = [[AVCaptureScreenInput alloc] initWithDisplayID:CGMainDisplayID()];
+            
+            screenInput.minFrameDuration = CMTimeMake(1, 60);
+            
+            [self.captureSession addInput:screenInput];
+        }
 
+        if (1) {
+            AVCaptureDevice *videoDevice = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo][0];
+            NSError *error;
+            AVCaptureDeviceInput *videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:videoDevice error:&error];
+            [self.captureSession addInput:videoInput];
+
+        }
+        
+        NSArray<AVCaptureDevice *> *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeAudio];
+        AVCaptureDevice *airpod = nil;
+        
+        for (AVCaptureDevice *device in devices) {
+            if([device.localizedName containsString:@"AirPod"]) {
+                airpod = device;
+                break;
+            }
+        }
+        
+        NSError *error;
+        AVCaptureDeviceInput *audioInput = [[AVCaptureDeviceInput alloc] initWithDevice:airpod error:&error];
+        [self.captureSession addInput:audioInput];
+        AVCaptureAudioDataOutput *audioOutput = [[AVCaptureAudioDataOutput alloc] init];
+        [self.captureSession addOutput:audioOutput];
+
+        
+		
 		AVCaptureVideoDataOutput *output = [[AVCaptureVideoDataOutput alloc] init];
 		[self.captureSession addOutput:output];
 		
@@ -39,14 +72,15 @@
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
 	// sampleBuffer contains screen cap, for me it's yuv
+	NSLog(@"buffer %@", sampleBuffer);
 }
 
 /*
-[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(captureError:) name:AVCaptureSessionRuntimeErrorNotification object:self.captureSession];
-
-- (void)captureError:(NSNotification *)notification {
-	NSLog(@"Error %@", notification);
-}
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(captureError:) name:AVCaptureSessionRuntimeErrorNotification object:self.captureSession];
+ 
+	- (void)captureError:(NSNotification *)notification {
+ NSLog(@"Error %@", notification);
+	}
  */
 
 @end

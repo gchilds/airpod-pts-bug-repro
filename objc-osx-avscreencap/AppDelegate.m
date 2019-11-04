1dcb2430147c8e4addb953f5f78cd65a4e2c2daa
Gordon Childs <gordon.childs@mail.com>
2019-11-04 18:01:05 +0100

    remove distractions

--- objc-osx-avscreencap/AppDelegate.m
+++ objc-osx-avscreencap/AppDelegate.m
@@ -10,10 +10,7 @@
 #import <AVFoundation/AVFoundation.h>
 #import "ScreenCapture.h"
 
-@interface AppDelegate () <AVCaptureVideoDataOutputSampleBufferDelegate>
-
-@property (nonatomic) AVCaptureSession *captureSession;
-@property (nonatomic) AVCaptureScreenInput *captureScreenInput;
+@interface AppDelegate ()
 
 @property (nonatomic) ScreenCapture *cappy;
 
@@ -22,58 +19,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	if (false) {
-		NSError *error;
-		[self createCaptureSession:&error];
-	} else {
-		self.cappy = [[ScreenCapture alloc] init];
-	}
+	self.cappy = [[ScreenCapture alloc] init];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-	// Insert code here to tear down your application
-}
-
-- (void)captureSessionRuntimeErrorDidOccur:(NSNotification *)not {
-	NSLog(@"Error %@", not);
-}
-
-- (BOOL)createCaptureSession:(NSError **)outError{
-	/* Create a capture session. */
-	self.captureSession = [[AVCaptureSession alloc] init];
-	if ([self.captureSession canSetSessionPreset:AVCaptureSessionPresetHigh])
-	{
-		/* Specifies capture settings suitable for high quality video and audio output. */
-		[self.captureSession setSessionPreset:AVCaptureSessionPresetHigh];
-	}
-	
-	/* Add the main display as a capture input. */
-	CGDirectDisplayID display = CGMainDisplayID();
-	self.captureScreenInput = [[AVCaptureScreenInput alloc] initWithDisplayID:display];
-	if ([self.captureSession canAddInput:self.captureScreenInput])
-	{
-		[self.captureSession addInput:self.captureScreenInput];
-	}
-	else
-	{   NSLog(@"nevyslo");
-		return NO;
-	}
-	
-	/* Register for notifications of errors during the capture session so we can display an alert. */
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(captureSessionRuntimeErrorDidOccur:) name:AVCaptureSessionRuntimeErrorNotification object:self.captureSession];
-	
-	
-	AVCaptureVideoDataOutput *output = [[AVCaptureVideoDataOutput alloc] init];
-	[self.captureSession addOutput:output];
-	
-	dispatch_queue_t queue = dispatch_queue_create("myQueue", NULL);
-	[output setSampleBufferDelegate:self queue:queue];
-	
-	[self.captureSession startRunning];
-	return YES;
-}
-
-- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
-	
-}
 @end
